52143207d98b0eb7f7d06f6bdb0dedbe2e4387c0
salwa107 <salwa.320240146@ejust.edu.eg>
2025-05-13 20:51:09 +0200

    ..

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-
+# database.py
 import sqlite3
 
 def get_connection():
@@ -8,17 +8,43 @@ def initialize_database():
     connection = get_connection()
     cursor = connection.cursor()
     
+    # Passenger table
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS Passenger (
         user_name TEXT PRIMARY KEY,
         email TEXT NOT NULL,
-        password_hash BLOB  NOT NULL,
+        password_hash BLOB NOT NULL,
         contact_number TEXT,
         passenger_id TEXT,
         age INTEGER,
         gender TEXT,
         passport_number TEXT,
         frequent_flyer_status TEXT
+    )
+    """)
+    
+    # Administrator table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Administrator (
+        user_name TEXT PRIMARY KEY,
+        email TEXT NOT NULL,
+        password_hash BLOB NOT NULL,
+        contact_number TEXT,
+        admin_id TEXT UNIQUE,
+        role TEXT
+    )
+    """)
+    
+    # CrewMember table
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS CrewMember (
+        user_name TEXT PRIMARY KEY,
+        email TEXT NOT NULL,
+        password_hash BLOB NOT NULL,
+        contact_number TEXT,
+        crew_id TEXT UNIQUE,
+        position TEXT,
+        airline TEXT
     )
     """)
     
