7804202b939fb864fe05fe98cf946138502b16c7
Mohamed Ahmed Ismail <mohamed.320240159@ejust.edu.eg>
2025-05-18 00:19:25 +0300

    newupdate before db connection

--- Log_in&Sign_up.py
+++ Log_in&Sign_up.py
@@ -2,6 +2,32 @@ from tkinter import *
 root = Tk()
 root.title("Airplan Reservation System Project")
 
+#the sign up method to save the new data to database
+def SignUp():
+    global signwindow
+    signwindow = Tk()
+    signwindow.title("Sign Up Window")
+    signwindow.geometry("400x400")
+
+    global entry1
+    global entry2
+    global entry3
+    global entry4
+    global entry5
+
+
+
+    confirmbut = Button(signwindow, text = "Confirm", command = newaccount).grid()
+
+#method of new account to db
+def newaccount():
+    #the database update
+
+
+    #crash the window
+    signwindow.destroy()
+
+
 #the checkmethod after clicking the cont button
 ### need lots if fixes ###
 def checkmethod():
@@ -18,38 +44,33 @@ def checkmethod():
 
 
 #defining the Label frames
-framebg = LabelFrame(root, padx = 100, pady = 100, bg = "Sky Blue")
-frame1 = LabelFrame(framebg, text = "Welcome" , padx = 200, pady = 200, bd = 1, bg = "Light Gray")
+framebg = LabelFrame(root, padx = 50, pady = 50, bg = "Sky Blue")
+frame1 = LabelFrame(framebg, text = "Welcome" , padx = 100, pady = 100, bd = 1, bg = "Light Gray")
 framebg.pack(padx = 25, pady = 25)
 frame1.pack(padx = 100 , pady = 100)
 
 #defining the Widgets
 entryName = Entry(frame1, bd = 1, width = 50)
+namelabel = Label(frame1, text = "Name", bg= "Light Gray")
 entryPassword = Entry(frame1, bd = 1 , width = 50)
+passwordlabel = Label(frame1, text = "Password", bg= "Light Gray")
 
 buttonEnd = Button(frame1, text = "Quit" , padx = 1 , pady = 1,command = root.quit)
 buttonCont = Button(frame1, text = "Continue" , padx = 1, pady = 1, command = checkmethod)
-buttonUp = Button(frame1, text = "Sign Up", padx = 1, pady = 1, command = None)
+buttonUp = Button(frame1, text = "Sign Up", padx = 1, pady = 1, command = SignUp)
 
-Space1 = Label(frame1, text = "", bg = "Light Gray")
-Space2 = Label(frame1, text = "", bg = "Light Gray")
 
-#test to memory the name in variables
-nnn = str()
-sss = str()
 
-entryName.insert(0, "Name: " + nnn)
-entryPassword.insert(0, "Password: " + sss)
 
 #Shoving Widgets
-entryName.grid(column = 1 , row = 1, columnspan= 2)
-Space1.grid(column = 1, row = 2)
-entryPassword.grid(column = 1 , row = 3, columnspan= 2)
-Space2.grid(column = 1, row = 4)
+entryName.grid(column = 1 , row = 1, columnspan= 2, pady = (0, 10))
+namelabel.grid(column = 0, row = 1, pady = (0, 10))
+entryPassword.grid(column = 1 , row = 2, columnspan= 2, pady = (0, 10))
+passwordlabel.grid(column = 0, row = 2, pady = (0, 10))
 
 
-buttonEnd.grid(column = 0, row = 5, columnspan = 2)
-buttonCont.grid(column = 1, row = 5, columnspan = 2)
-buttonUp.grid(row = 5, column = 2, columnspan = 2)
+buttonEnd.grid(column = 0, row = 3, columnspan = 2)
+buttonCont.grid(column = 0, row = 3, columnspan = 3)
+buttonUp.grid(column = 2, row = 3, columnspan = 1)
 
 root.mainloop()
